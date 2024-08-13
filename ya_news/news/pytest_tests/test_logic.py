8ad91b5c54f43fdb494e974356803a31ce84214e
alexander voznyy <a.voznyy@vk.team>
2024-08-13 23:24:22 +0300

    ya_news done

--- ya_news/news/pytest_tests/test_logic.py
+++ ya_news/news/pytest_tests/test_logic.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from news.models import News, Comment
 from pytest_django.asserts import assertRedirects
 from news.forms import BAD_WORDS, WARNING
-
+from http import HTTPStatus
 
 @pytest.mark.django_db
 def test_anonymous_user_cant_create_note(client,news, form_data):
@@ -26,24 +26,63 @@ def test_user_cant_comment_bad_words(author_client,news):
     form_data={'text': f'Привет {BAD_WORDS[0]}',}
     url = reverse('news:detail', args=(news.pk,))
     response = author_client.post(url, data=form_data)
-    # print(response.context['form'].errors.keys())
-    # raise ValidationError(WARNING) return text
     assert WARNING in response.context['form'].errors['text']
     # Считаем количество заметок в БД, ожидаем 0 заметок.
     assert Comment.objects.count() == 0
 
 @pytest.mark.django_db
-def test_auth_user_send_comment(author_client,news,author):
+def test_auth_user_send_comment(author_client,news,author,):
     """
     Пункт 3 Авторизованный пользователь может отправить комментарий.
     """
     form_data={'text': f'Привет я {author}',}
     url = reverse('news:detail', args=(news.pk,))
     response = author_client.post(url, data=form_data)
-    # raise ValidationError(WARNING) return text
-    #assert WARNING in response.context['form'].errors['text']
-    # Считаем количество заметок в БД, ожидаем 0 заметок.
     assert Comment.objects.count() == 1
-    assert Comment.objects.get().author == author
-    assert Comment.objects.get().text == form_data['text']
+    comment_from_db = Comment.objects.get()
+    assert comment_from_db.text == form_data['text']
+    assert comment_from_db.author == author
+    assert comment_from_db.news == news
 
+
+@pytest.mark.django_db
+def test_auth_user_send_comment(author_client,news,comment,pk_for_args,author):
+    """
+    Пункт 4 Авторизованный пользователь может редактировать или удалять свои комментарии.
+    """
+    # Новый текст
+    form_data={'text': f'Привет я {author}',}
+    # Редактирование коментария
+    url = reverse('news:edit', args=pk_for_args)
+    # В POST-запросе на адрес редактирования заметки
+    # отправляем form_data - новые значения для полей заметки   
+    response = author_client.post(url, data=form_data)
+    assert response.status_code == HTTPStatus.FOUND
+    assertRedirects(response, reverse('news:detail', args=pk_for_args) + '#comments')
+    comment.refresh_from_db()
+    assert comment.text == form_data['text']
+    assert comment.author == author
+    assert comment.news == news
+    # Удаление коментария 
+    response = author_client.post(reverse('news:delete', args=pk_for_args))
+    assert response.status_code == HTTPStatus.FOUND
+    #test_auth_user_send_comment <HttpResponseRedirect status_code=302, "text/html; charset=utf-8", url="/news/1/#comments">
+    assertRedirects(response, reverse('news:detail',args=pk_for_args) + "#comments")
+    assert Comment.objects.count() == 0
+
+@pytest.mark.django_db
+def test_auth_user_send_comment(not_author_client,news,comment,pk_for_args,not_author):
+    """
+    Пункт 5 Авторизованный пользователь не может редактировать или удалять чужие комментарии
+    """
+    form_data={'text': f'Привет я {not_author}',}
+    # Редактирование коментария
+    url = reverse('news:edit', args=pk_for_args)
+    response = not_author_client.post(url, data=form_data)
+    # Проверяем, что страница не найдена:
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    comment_from_db = Comment.objects.get(id=comment.id)
+    assert comment.text == comment_from_db.text
+    response = not_author_client.post(reverse('news:delete', args=pk_for_args))
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert Comment.objects.count() == 1
