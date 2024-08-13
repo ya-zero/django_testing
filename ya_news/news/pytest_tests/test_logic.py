c68aff371527dbc7ea21bec8489dbe9a491f2aa5
alexander voznyy <a.voznyy@vk.team>
2024-08-13 11:20:11 +0300

    logical_add_punkt3

--- ya_news/news/pytest_tests/test_logic.py
+++ ya_news/news/pytest_tests/test_logic.py
@@ -2,6 +2,8 @@ import pytest
 from django.urls import reverse
 from news.models import News, Comment
 from pytest_django.asserts import assertRedirects
+from news.forms import BAD_WORDS, WARNING
+
 
 @pytest.mark.django_db
 def test_anonymous_user_cant_create_note(client,news, form_data):
@@ -15,4 +17,33 @@ def test_anonymous_user_cant_create_note(client,news, form_data):
     assertRedirects(response, expected_url)
     # Считаем количество заметок в БД, ожидаем 0 заметок.
     assert Comment.objects.count() == 0
+
+@pytest.mark.django_db
+def test_user_cant_comment_bad_words(author_client,news):
+    """
+    Пункт 2 Если комментарий содержит запрещённые слова, он не будет опубликован, а форма вернёт ошибку.
+    """
+    form_data={'text': f'Привет {BAD_WORDS[0]}',}
+    url = reverse('news:detail', args=(news.pk,))
+    response = author_client.post(url, data=form_data)
+    # print(response.context['form'].errors.keys())
+    # raise ValidationError(WARNING) return text
+    assert WARNING in response.context['form'].errors['text']
+    # Считаем количество заметок в БД, ожидаем 0 заметок.
+    assert Comment.objects.count() == 0
+
+@pytest.mark.django_db
+def test_auth_user_send_comment(author_client,news,author):
+    """
+    Пункт 3 Авторизованный пользователь может отправить комментарий.
+    """
+    form_data={'text': f'Привет я {author}',}
+    url = reverse('news:detail', args=(news.pk,))
+    response = author_client.post(url, data=form_data)
+    # raise ValidationError(WARNING) return text
+    #assert WARNING in response.context['form'].errors['text']
+    # Считаем количество заметок в БД, ожидаем 0 заметок.
+    assert Comment.objects.count() == 1
+    assert Comment.objects.get().author == author
+    assert Comment.objects.get().text == form_data['text']
 
