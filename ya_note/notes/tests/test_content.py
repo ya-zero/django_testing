a1924b6907b8529c8ed74328c9ff036509cc037e
alexander voznyy <a.voznyy@vk.team>
2024-08-16 13:55:54 +0300

    add test_content

---
+++ ya_note/notes/tests/test_content.py
@@ -0,0 +1,64 @@
+# отдельная заметка передаётся на страницу со списком заметок в списке object_list в словаре context; +
+# в список заметок одного пользователя не попадают заметки другого пользователя;
+# на страницы создания и редактирования заметки передаются формы.
+
+# news/tests/test_content.py
+from django.test import TestCase
+# Импортируем функцию для определения модели пользователя.
+from django.contrib.auth import get_user_model
+# Импортируем функцию reverse(), она понадобится для получения адреса страницы.
+from django.urls import reverse
+# Импортируем класс Заметок.
+from notes.models import Note
+from notes.forms import NoteForm
+
+
+# Получаем модель пользователя.
+User = get_user_model()
+
+
+class TestContent(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.author = User.objects.create(username='Автор')
+        cls.no_author = User.objects.create(username='Не Автор')
+        cls.note = Note.objects.create(
+            title='Заголовок',
+            slug='zagolovok',
+            author=cls.author,
+            text='Текст комментария'
+        )
+        cls.note_no_author = Note.objects.create(
+            title='Заголовок',
+            slug='zagolovok_',
+            author=cls.no_author,
+            text='Текст комментария'
+        )
+        cls.detail_url = reverse('notes:detail', args=(cls.note.slug,))
+
+    def test_object_list(self):
+        self.client.force_login(self.author)
+        response = self.client.get(reverse('notes:list'))
+        # Получаем список объектов из словаря контекста.
+        object_list = response.context['object_list']
+        self.assertIn(self.note, object_list)
+
+    def test_object_list_another_user(self):
+        self.client.force_login(self.author)
+        response = self.client.get(reverse('notes:list'))
+        object_list = response.context['object_list']
+        self.assertNotIn(self.note_no_author, object_list)
+
+    def test_authorized_client_has_form(self):
+        urls = (
+            ('notes:add', None),
+            ('notes:edit', (self.note.slug,)),
+        )
+        # Авторизуем клиент при помощи ранее созданного пользователя.
+        self.client.force_login(self.author)
+        for name, args in urls:
+            with self.subTest(name=name):
+                response = self.client.get(reverse(name, args=args))
+                self.assertIn('form', response.context)
+                # Проверим, что объект формы соответствует нужному классу формы.
+                self.assertIsInstance(response.context['form'], NoteForm)
--- ya_note/notes/tests/test_routes.py
+++ ya_note/notes/tests/test_routes.py
@@ -30,7 +30,11 @@ class TestRoutes(TestCase):
         )
 
     def test_guest_pages_availability(self):
-        """Главная страница доступна анонимному пользователю."""
+        """
+        Пункт 1. Главная страница доступна анонимному пользователю.
+        Пункт 5. Страницы регистрации пользователей,входа в учётную запись 
+        и выхода из неё доступны всем пользователям.
+        """
         urls = (
             ('notes:home', None),
             ('users:login', None),
@@ -44,8 +48,8 @@ class TestRoutes(TestCase):
                 self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_auth_pages_availability(self):
-        """Аутентифицированному пользователю доступна страница.
-
+        """
+        Аутентифицированному пользователю доступна страница
         со списком заметок notes/,
         страница успешного добавления заметки done/,
         страница добавления новой заметки add/.
@@ -61,3 +65,54 @@ class TestRoutes(TestCase):
                 url = reverse(name)
                 response = self.client.get(url)
                 self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_availability_for_note_edit_and_delete(self):
+        """
+        Страницы отдельной заметки, удаления и редактирования заметки
+        доступны. только автору заметки. Если на эти страницы попытается
+        зайти другой пользователь — вернётся ошибка 404.
+        """
+        users_statuses = (
+            (self.author, HTTPStatus.OK),
+            (self.notauthor, HTTPStatus.NOT_FOUND),
+        )
+        urls = (
+            ('notes:detail', (self.note.slug,)),
+            ('notes:edit', (self.note.slug,)),
+            ('notes:delete', (self.note.slug,)),
+        )
+        for user, status in users_statuses:
+            # Логиним пользователя в клиенте:
+            self.client.force_login(user)
+            # Для каждой пары "пользователь - ожидаемый ответ"
+            # перебираем имена тестируемых страниц:
+            for name, args in urls:
+                with self.subTest(user=user, name=name):
+                    url = reverse(name, args=args)
+                    response = self.client.get(url)
+                    self.assertEqual(response.status_code, status)
+
+    def test_redirect_for_anonymous_client(self):
+        """
+        При попытке перейти на страницу списка заметок, страницу
+        успешного добавления записи, страницу добавления заметки,
+        отдельной заметки, редактирования или удаления заметки
+        анонимный пользователь перенаправляется на страницу логина.
+        """
+        urls = (
+            ('notes:list', None),
+            ('notes:success', None),
+            ('notes:add', None),
+            ('notes:edit', (self.note.slug,)),
+            ('notes:delete', (self.note.slug,)),
+        )
+        # Сохраняем адрес страницы логина:
+        login_url = reverse('users:login')
+        # В цикле перебираем имена страниц, с которых ожидаем редирект:
+        for name, args in urls:
+            with self.subTest(name=name):
+                url = reverse(name, args=args)
+                redirect_url = f'{login_url}?next={url}'
+                response = self.client.get(url, args=args)
+                # Проверяем, что редирект приведёт именно на указанную ссылку.
+                self.assertRedirects(response, redirect_url)
