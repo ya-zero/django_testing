9dc7029ee8952ab435252a8b5cc47cd2096038f2
alexander voznyy <a.voznyy@vk.team>
2024-08-16 14:15:47 +0300

    fix ya_note

--- ya_note/notes/tests/test_content.py
+++ ya_note/notes/tests/test_content.py
@@ -1,8 +1,4 @@
-# отдельная заметка передаётся на страницу со списком заметок в списке object_list в словаре context; +
-# в список заметок одного пользователя не попадают заметки другого пользователя;
-# на страницы создания и редактирования заметки передаются формы.
-
-# news/tests/test_content.py
+# notes/tests/test_content.py
 from django.test import TestCase
 # Импортируем функцию для определения модели пользователя.
 from django.contrib.auth import get_user_model
@@ -37,6 +33,10 @@ class TestContent(TestCase):
         cls.detail_url = reverse('notes:detail', args=(cls.note.slug,))
 
     def test_object_list(self):
+        """
+        Отдельная заметка передаётся на страницу со
+        списком заметок в списке object_list в словаре context;
+        """
         self.client.force_login(self.author)
         response = self.client.get(reverse('notes:list'))
         # Получаем список объектов из словаря контекста.
@@ -44,12 +44,17 @@ class TestContent(TestCase):
         self.assertIn(self.note, object_list)
 
     def test_object_list_another_user(self):
+        """
+        В список заметок одного пользователя не попадают
+        заметки другого пользователя
+        """
         self.client.force_login(self.author)
         response = self.client.get(reverse('notes:list'))
         object_list = response.context['object_list']
         self.assertNotIn(self.note_no_author, object_list)
 
     def test_authorized_client_has_form(self):
+        """На страницы создания и редактирования заметки передаются формы."""
         urls = (
             ('notes:add', None),
             ('notes:edit', (self.note.slug,)),
@@ -60,5 +65,5 @@ class TestContent(TestCase):
             with self.subTest(name=name):
                 response = self.client.get(reverse(name, args=args))
                 self.assertIn('form', response.context)
-                # Проверим, что объект формы соответствует нужному классу формы.
+                # Проверим, что объект формы соответствует классу формы.
                 self.assertIsInstance(response.context['form'], NoteForm)
